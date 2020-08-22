cf3472cf9d3270022acbc89359d5194ec56da2a7
Kadir Ince <kadirincedev@gmail.com>
2020-08-22 21:08:38 +0300

    Initial Commit

---
+++ Fitness Dashboard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Fitness Dashboard
+//
+//  Created by Kadir on 22.08.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Fitness Dashboard/Fitness_DashboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fitness_DashboardApp.swift
+//  Fitness Dashboard
+//
+//  Created by Kadir on 22.08.2020.
+//
+
+import SwiftUI
+
+@main
+struct Fitness_DashboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
